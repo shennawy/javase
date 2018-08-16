357f7cc07029fc71365a14db51e18e3377713a4d
Shennawy <shennawy25@gmail.com>
2018-08-16 17:13:56 +0200

    Finished CRUD operations on OE project

--- oop/oe/Main.java
+++ oop/oe/Main.java
@@ -16,39 +16,42 @@ import javase.oop.oe.product.SoftwareHandler;
 public class Main {
 
     public static void main(String[] args) {
-        Company comp = new Company("Sheraton", 103, "CIB", "010", "Muhammad", 10);
-        Individual individual = new Individual("Heliopolis", 102, "Karim", "012", "1");
+        //   Company comp = new Company("Sheraton", 103, "CIB", "010", "Muhammad", 10);
+        //  Individual individual = new Individual("Heliopolis", 102, "Karim", "012", "1");
 
         //  CustomerHandler.insertCustomer(comp);
         // CustomerHandler.insertCustomer(individual);
 
-        Software sw1 = new Software("Windows", 100, "Windows 10", 10.5, "Meh");
-        Hardware hw1 = new Hardware("CPU", 101, "Intel", 550.0, 1);
-        Hardware hw2 = new Hardware("GPU", 102, "AMD", 600.0, 1);
-        Manual m1 = new Manual("How to", 6, "Cook", 12.0, "Medhat");
+        // Software sw1 = new Software("Windows", 100, "Windows 10", 10.5, "Meh");
+        // Hardware hw1 = new Hardware("CPU", 101, "Intel", 550.0, 1);
+        // Hardware hw2 = new Hardware("GPU", 102, "AMD", 600.0, 1);
+        //   Manual m1 = new Manual("How to", 6, "Cook", 12.0, "Medhat");
 
         // SoftwareHandler.insertSoftware(sw1);
         // HardwareHandler.insertHardware(hw1);
         // HardwareHandler.insertHardware(hw2);
 
-        Date d1 = new Date();
-        Order o1 = new Order(0, d1);
+        //  Date d1 = new Date();
+        // Order o1 = new Order(0, d1);
 
-        o1.setCustomer(comp);
+        //   o1.setCustomer(comp);
 
-        o1.addOrderItems(hw1);
-        o1.addOrderItems(hw1);
-        o1.addOrderItems(hw1);
-        o1.addOrderItems(hw2);
-        o1.addOrderItems(sw1);
-       // o1.addOrderItems(m1);
+        //   o1.addOrderItems(hw1);
+        // o1.addOrderItems(hw1);
+        // o1.addOrderItems(hw1);
+        // o1.addOrderItems(hw2);
+        //  o1.addOrderItems(sw1);
+        // o1.addOrderItems(m1);
         //o1.addOrderItems(m1);
 
         // System.out.println("Total = " + o1.getOrderTotal());
-        o1.getOrderDetails();
+        //  o1.getOrderDetails();
 
-        OrderHandler.insertOrder(o1);
+        //  OrderHandler.insertOrder(o1);
 
+        Order o2 = OrderHandler.getOrderById(107);
+
+        o2.getOrderDetails();
     }
 
 }
--- oop/oe/order/OrderHandler.java
+++ oop/oe/order/OrderHandler.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javase.oop.oe.db.DBConnectionFactory;
+import javase.oop.oe.product.HardwareHandler;
+import javase.oop.oe.product.SoftwareHandler;
 
 public class OrderHandler {
 
@@ -34,8 +37,6 @@ public class OrderHandler {
                 pstmtOrderItems.setInt(1, oi.getQuantity());
                 pstmtOrderItems.setInt(2, oi.getProduct().getProductId());
                 pstmtOrderItems.executeUpdate();
-                
-                System.out.println("Debug: "+ oi.getQuantity() + oi.getProduct().getProductId());
             }
 
             conn.commit();
@@ -44,6 +45,56 @@ public class OrderHandler {
             e.printStackTrace();
         }
 
+    }
+
+    public static Order getOrderById(int orderId) {
+        Order order = null;
+        OrderItem oi = null;
+        String sqlOrder = "SELECT ORDER_DATE, ORDER_TOTAL, CUSTOMER_ID FROM ORDERS WHERE ORDER_ID LIKE " + orderId;
+        String sqlOrderItems = "SELECT ORDER_ITEM_QTY, PRODUCT_ID FROM ORDER_ITEMS WHERE ORDER_ID LIKE " + orderId;
+        PreparedStatement pstmtOrder = null;
+        PreparedStatement pstmtOrderItems = null;
+        ResultSet rsOrder = null;
+        ResultSet rsOrderItems = null;
+        Connection conn = DBConnectionFactory.initConn();
+        int productId;
+        int orderItemQTY;
+
+        try {
+            pstmtOrder = conn.prepareStatement(sqlOrder);
+            rsOrder = pstmtOrder.executeQuery();
+
+            pstmtOrderItems = conn.prepareStatement(sqlOrderItems);
+            rsOrderItems = pstmtOrderItems.executeQuery();
+
+            int lineNbr = 0;
+            while (rsOrder.next()) {
+                order = new Order(orderId, rsOrder.getDate("ORDER_DATE"));
+                while (rsOrderItems.next()) {
+                    productId = rsOrderItems.getInt("PRODUCT_ID");
+                    orderItemQTY = rsOrderItems.getInt("ORDER_ITEM_QTY");
+
+                    if (HardwareHandler.getHardwareById(productId) != null) {
+
+                        oi = new OrderItem(lineNbr, HardwareHandler.getHardwareById(productId), orderItemQTY);
+                        order.addOrderItems(HardwareHandler.getHardwareById(productId));
+                    } else if (SoftwareHandler.getSoftwareById(productId) != null) {
+
+                        oi = new OrderItem(lineNbr, SoftwareHandler.getSoftwareById(productId), orderItemQTY);
+                        order.addOrderItems(SoftwareHandler.getSoftwareById(productId));
+                    }
+                    // order.addOrderItems(product);
+                    lineNbr++;
+                    System.out.println("Product ID: " + productId);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in OrderHandler.getOrderById");
+            e.printStackTrace();
+        }
+
+        return order;
     }
 
 }
