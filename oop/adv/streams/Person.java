1c1f8973ec7cb6435a7363fd2133faf88304cdb4
Shennawy <shennawy25@gmail.com>
2018-08-28 12:38:20 +0200

    Builder design pattern example in  Person class

--- oop/adv/streams/Person.java
+++ oop/adv/streams/Person.java
@@ -15,7 +15,19 @@ public class Person {
     private String city;
     private String state;
     private String code;
-
+    // Takes Builder object as param, must be set to private
+    private Person(Builder builder) {
+        this.givenName = builder.givenName;
+        this.surName = builder.surName;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.eMail = builder.eMail;
+        this.phone = builder.phone;
+        this.address = builder.address;
+        this.city = builder.city;
+        this.state = builder.state;
+        this.code = builder.code;
+    }
 
     public String getGivenName() {
         return givenName;
@@ -64,5 +76,73 @@ public class Person {
 
     }
 
+    public static class Builder {
 
+        private String givenName;
+        private String surName;
+        private int age;
+        private Gender gender;
+        private String eMail;
+        private String phone;
+        private String address;
+        private String city;
+        private String state;
+        private String code;
+
+        // Setters to create Builder Object first
+        public Builder setGivenName(String givenName) {
+            this.givenName = givenName;
+            return this;
+        }
+
+        public Builder setSurName(String surName) {
+            this.surName = surName;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder setEMail(String eMail) {
+            this.eMail = eMail;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder setState(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder setCode(String code) {
+            this.code = code;
+            return this;
+        }
+        // To return a new Person
+        public Person build() {
+            return new Person(this);
+        }
+
+    }
 }
