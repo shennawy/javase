846a2ec3233759fb08c322b4fbadd906e6c00e4d
Shennawy <shennawy25@gmail.com>
2018-08-29 11:19:48 +0200

    Finished lambda_01

--- oop/adv/Lab10/lambda_01/MapTest.java
+++ oop/adv/Lab10/lambda_01/MapTest.java
@@ -15,7 +15,10 @@ public class MapTest {
         // Calculate sales tax for CA Transactions
         System.out.println("=== Widget Pro Sales Tax in CA ===");
 
-        
-        
+        tList.stream()
+            .filter(t->t.getState().equals(State.CA))
+            .filter(t -> t.getProduct().equals("Widget Pro"))
+           .map(t-> t.getTransactionTotal() * TaxRate.byState(t.getState()))
+            .forEach(t -> System.out.printf("Txn Tax: $%,9.2f \n", t));
     }
 }
--- oop/adv/Lab10/lambda_01/PeekTest.java
+++ oop/adv/Lab10/lambda_01/PeekTest.java
@@ -12,15 +12,20 @@ public class PeekTest {
     public static void main(String[] args) {
     
     List<SalesTxn> tList = SalesTxn.createTxnList();
-    Consumer<SalesTxn> taxReport = 
-      t -> System.out.printf("Id: " + t.getTxnId() 
-        + " Buyer: " + t.getBuyerName() + 
-        " Txn amt: $%,9.2f ", t.getTransactionTotal());
+
+        Consumer<SalesTxn> taxReport = 
+          t -> System.out.printf("Id: " + t.getTxnId() 
+            + " Buyer: " + t.getBuyerName() + 
+            " Txn amt: $%,9.2f ", t.getTransactionTotal());
     
     // Write report using peek
     System.out.println("=== Widget Pro Sales Tax in CA ===");
+tList.stream()
+        .filter(t->t.getState().equals(State.CA))
+        .filter(t -> t.getProduct().equals("Widget Pro"))
+        .peek(taxReport)
+        .map(t-> t.getTransactionTotal() * TaxRate.byState(t.getState()))
+        .forEach(t -> System.out.printf("Txn Tax: $%,9.2f \n", t));
 
-        
-        
     }
 }
