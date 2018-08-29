58d7674063fee5c2a2036a64207f4618b688097c
Shennawy <shennawy25@gmail.com>
2018-08-29 12:01:59 +0200

    Finished Lambda_04

--- oop/adv/Lab10/lambda_04/CalcTest.java
+++ oop/adv/Lab10/lambda_04/CalcTest.java
@@ -1,7 +1,10 @@
 package javase.oop.adv.Lab10.lambda_04;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.function.Consumer;
+import java.util.stream.DoubleStream;
 
 /**
  *
@@ -17,15 +20,31 @@ public class CalcTest {
         System.out.println("=== Transactions Totals ===");
         // Replace with stream
         double t1 = 0;
+        double t2 = 0;
+        double t3 = 0;
+        t1 += tList.stream()
+            .filter(t -> t.getBuyerName().equals("Radio Hut"))
+            .mapToDouble(t-> t.getTransactionTotal())
+            .sum();
+            
+        t2 += tList.stream()
+            .filter(t -> t.getBuyerName().equals("PriceCo"))
+            .mapToDouble(t->t.getTransactionTotal())
+            .sum();
+        
+        t3 += tList.stream()
+            .filter(t -> t.getBuyerName().equals("Best Deals"))
+            .mapToDouble(t->t.getTransactionTotal())
+            .sum();
         
         System.out.printf("Radio Hut Total: $%,9.2f%n", t1);
         
         // Replace with Stream
-        double t2 = 0;
+        
         
         System.out.printf("PriceCo Total: $%,9.2f%n", t2);
         // Replace with stream
-        double t3 = 0;
+        
         
         System.out.printf("Best Deals Total: $%,9.2f%n", t3);
         
@@ -34,6 +53,12 @@ public class CalcTest {
         // Replace with Stream
         OptionalDouble a1 = OptionalDouble.of(0);
         
+  
+        a1 = tList.stream()
+            .filter(t-> t.getProduct().equals("Widget"))
+            .mapToDouble(t-> t.getUnitCount())
+            .average();
+        
         if (a1.isPresent()){
             System.out.printf("Widget Pro Avg: %,9.0f%n", a1.getAsDouble());
         }
@@ -41,6 +66,10 @@ public class CalcTest {
         // Replace with stream
         OptionalDouble a2 = OptionalDouble.of(0);
         
+        a2 =  tList.stream()
+            .filter(t-> t.getProduct().equals("Widget Pro"))
+            .mapToDouble(t-> t.getUnitCount())
+            .average();
         if (a2.isPresent()){
             System.out.printf("Widget Avg: %,9.0f%n", a2.getAsDouble());        
         }
