9b9cab3d555d8639c7c1206caf3bda0833f3f09d
Shennawy <shennawy25@gmail.com>
2018-08-29 11:45:19 +0200

    Added Lambda_04

---
+++ oop/adv/Lab10/lambda_04/Main.java
@@ -0,0 +1,14 @@
+package javase.oop.adv.Lab10.lambda_04;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CalcTest.main(args);
+      
+  }
+}
