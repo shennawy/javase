1cd7098bfff8a2ee38df7cd244dfb051d8c62817
Shennawy <shennawy25@gmail.com>
2018-08-29 10:35:48 +0200

    Added Examples of Lab10

---
+++ oop/adv/Lab10/lambda_01/Main.java
@@ -0,0 +1,15 @@
+package javase.oop.adv.Lab10.lambda_01;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      MapTest.main(args);
+      PeekTest.main(args);
+      
+  }
+}
---
+++ oop/adv/Lab10/lambda_01/MapTest.java
@@ -0,0 +1,21 @@
+package javase.oop.adv.Lab10.lambda_01;
+
+import java.util.List;
+
+/**
+ *
+ * @author oracle
+ */
+public class MapTest {
+    
+    public static void main(String[] args) {
+
+        List<SalesTxn> tList = SalesTxn.createTxnList();
+   
+        // Calculate sales tax for CA Transactions
+        System.out.println("=== Widget Pro Sales Tax in CA ===");
+
+        
+        
+    }
+}
