9020736f8cd1cef95e8b6a033ce6bd3beed003fc
Mahmoud Elshennawy <shennawy25@gmail.com>
2018-08-30 13:52:48 +0200

    Created Runnable, Callable examples in javase.oop.adv.threads.thread_pool

---
+++ oop/adv/threads/thread_pool/TestThreadPoolRunnable.java
@@ -0,0 +1,34 @@
+package javase.oop.adv.threads.thread_pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestThreadPoolRunnable {
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+
+        for (int i = 0; i < 20; i++) { /// Create 20 tasks for Threads in Thread pool
+            Thread task = new Thread(new TaskThreadRunnable("Task " + i));
+            executor.submit(task);
+        }
+
+        Thread task1 = new Thread(new TaskThreadRunnable("Task 1 new "));
+        Thread task2 = new Thread(new TaskThreadRunnable("Task 2 new "));
+        Thread task3 = new Thread(new TaskThreadRunnable("Task 3 new "));
+        executor.submit(task1);
+        executor.submit(task2);
+        executor.submit(task3);
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("All Tasks are done");
+        System.out.println("In time : " + (endTime - startTime));
+
+
+    }
+}
