cdcd5f583ef1967c0d0604959a5d009ce12b86a1
Mahmoud Elshennawy <shennawy25@gmail.com>
2018-08-30 13:08:57 +0200

    Added MyBus.java, PassengerBus.java, BusReservationDemo.java to javase.oop.adv.threads.bus_reservation

---
+++ oop/adv/threads/bus_reservation/MyBus.java
@@ -0,0 +1,38 @@
+package javase.oop.adv.threads.bus_reservation;
+
+public class MyBus implements Runnable {
+    private int totalSeatsAvailable = 2;
+
+    @Override
+    public void run() {
+                  
+     PassengerThread pt = (PassengerThread) Thread.currentThread();  
+        
+                  
+     boolean ticketsBooked = 
+          this.bookTickets(pt.getSeatsNeeded(), pt.getName());
+    
+            if(ticketsBooked==true){
+                    System.out.println("CONGRATS "+Thread.currentThread().getName()+" .. The number of seats requested("+pt.getSeatsNeeded()+")  are BOOKED");
+            }else{
+                    System.out.println("Sorry Mr. "+Thread.currentThread().getName()+" .. The number of seats requested("+pt.getSeatsNeeded()+")  are not available");       
+            }
+    }
+    
+    public synchronized boolean bookTickets(int seats, String name){
+        System.out.println("Welcome to HappyBus "+
+            Thread.currentThread().getName()+
+                  " Number of Tickets Available are:"+this.getTotalSeatsAvailable());
+        
+        if (seats>this.getTotalSeatsAvailable()) {
+                                return false;
+                        } else {
+                                totalSeatsAvailable = totalSeatsAvailable-seats;
+                                return true;
+                        }
+    }
+
+    public int getTotalSeatsAvailable() {
+        return totalSeatsAvailable;
+    }
+}
